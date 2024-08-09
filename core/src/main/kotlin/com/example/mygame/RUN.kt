4cc8a134ccfeba255630d36c11e2c131b91498bf
이지훈 <155136709+dlwlgnsrhy@users.noreply.github.com>
2024-08-09 10:42:01 +0900

    Initial commit

---
+++ core/src/main/kotlin/com/example/mygame/RUN.kt
@@ -0,0 +1,6 @@
+package com.example.mygame
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class RUN : ApplicationAdapter()
